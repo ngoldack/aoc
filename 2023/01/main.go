34722763b7d4276e1f73473f66b554c2e6cf9211
ngoldack <nicolas.goldack@proton.me>
2023-12-11 14:03:44 +0100

    feat(2023): added day 1

---
+++ 2023/01/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	file, err := os.ReadFile("input.txt")
+	if err != nil {
+		panic(err)
+	}
+	content := strings.Split(string(file), "\n")
+
+	sum := 0
+	for i, v := range content {
+		x, err := decode(v)
+		if err != nil {
+			panic(err)
+		}
+		sum += x
+	}
+
+	fmt.Println(sum)
+}
+
+func decode(s string) (int, error) {
+	first, last := -1, -1
+
+	for _, v := range s {
+		if v >= 48 && v <= 57 {
+			if first < 0 {
+				first = int(v) - 48
+			}
+
+			last = int(v) - 48
+		}
+	}
+
+	if first < 0 || last < 0 {
+		return -1, fmt.Errorf("negative value: first: %v, last: %v", first, last)
+	}
+
+	return first*10 + last, nil
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+# AOC: Advent of Code
+
+Advent of Code.
+
+## 2023
+
+This year I'm completing the challenges in Go.
